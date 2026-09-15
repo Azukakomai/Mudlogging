668c5590c240585728651263330417ff282ef1aa
Azka Rahman <mohammadazkakhairurrahman@gmail.com>
2026-09-15 17:32:03 +0700

    app build

--- app.py
+++ app.py
@@ -1951,5 +1951,7 @@ def open_browser():
 
 
 if __name__ == "__main__":
+    import multiprocessing
+    multiprocessing.freeze_support()
     threading.Thread(target=open_browser, daemon=True).start()
     app.run(debug=False, port=8051)
